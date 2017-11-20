cd63b5a98c2f95847a8d6626299d14972f4e57d5
caio.cvac <caio.cvac@dpf.gov.br>
2017-11-20 13:07:27 -0200

    Aula 09

--- src/br/com/caelum/financas/modelo/Movimentacao.java
+++ src/br/com/caelum/financas/modelo/Movimentacao.java
@@ -12,8 +12,11 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.NamedQuery;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+
+@NamedQuery(query="select distinct avg(m.valor) from Movimentacao m where m.conta = :pConta" + " and m.tipo = :pTipo" + " group by m.data", name="MediasPorDiaETipo")
 
 @Entity
 public class Movimentacao {
--- src/br/com/caelum/financas/teste/TesteFuncoesJPQL.java
+++ src/br/com/caelum/financas/teste/TesteFuncoesJPQL.java
@@ -3,7 +3,6 @@ package br.com.caelum.financas.teste;
 import java.util.List;
 
 import javax.persistence.EntityManager;
-import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 
 import br.com.caelum.financas.modelo.Conta;
@@ -19,16 +18,13 @@ public class TesteFuncoesJPQL {
 		
 		Conta conta = new Conta();
 		conta.setId(2);
-		
-		String jpql = "select distinct avg(m.valor) from Movimentacao m where m.conta=:pConta"+
-				" and m.tipoMovimentacao = :pTipo"+
-				" group by m.data";
-		
-		TypedQuery<Double> query = em.createQuery(jpql, Double.class);
-		query.setParameter("pConta", conta);
-		query.setParameter("pTipo", TipoMovimentacao.SAIDA);
-		
-		List<Double> medias = query.getResultList();
+
+		TypedQuery<Double> typedQuery = em.createNamedQuery("MediasPorDiaETipo", Double.class);
+
+		typedQuery.setParameter("pConta", conta);
+		typedQuery.setParameter("pTipo", TipoMovimentacao.SAIDA);
+
+		List<Double> medias = typedQuery.getResultList();;
 		
 		for (Double double1 : medias) {
 			
